f7fa7944473ebbf4433972a9a1fe002c9867b607
saad09092012 <msaadatif@gmail.com>
2025-10-25 13:47:21 +0400

    homework

---
+++ Homework.py
@@ -0,0 +1,12 @@
+# importing calendar module
+import calendar
+
+yy = 2025  # year
+mm = 5  # month
+
+# To take month and year input from the user
+# yy = int(input("Enter year: "))
+# mm = int(input("Enter month: "))
+
+# display the calendar
+print(calendar.month(yy, mm))
